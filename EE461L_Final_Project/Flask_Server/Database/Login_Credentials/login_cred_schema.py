afbeff41e006b44511315311e016d136fa924b66
David Janosky <davidjanosky@gmail.com>
2021-03-04 10:14:32 -0600

    Working on login stuff, todo add DB accessors to servicer

--- EE461L_Final_Project/Flask_Server/Database/Login_Credentials/login_cred_schema.py
+++ EE461L_Final_Project/Flask_Server/Database/Login_Credentials/login_cred_schema.py
@@ -1,1 +1,6 @@
+from marshmallow import Schema, fields
+
+class LoginSetSchema(Schema):
+    user_id = fields.Str(required=True)
+    password = fields.Str(required=True)
 
--- EE461L_Final_Project/Flask_Server/Database/Login_Credentials/login_cred_service.py
+++ EE461L_Final_Project/Flask_Server/Database/Login_Credentials/login_cred_service.py
@@ -2,14 +2,33 @@ import hashlib
 import base64
 import bcrypt
 
+from ..db_entry import DataSet
+from ..mongo import MongoEntry
+from .login_cred_schema import LoginSetSchema
+
 SALT_ROUNDS = 16
 
-def get_hashed_password(plain_text_password):
-  # Hash a password for the first time
-  # Using bcrypt, the salt is saved into the hash itself preventing rainbow table attacks
-  # We use sha256 and encode using baase 64 to bypass the max length problems of blowfish
-  return bcrypt.hashpw(base64.b64encode(hashlib.sha256(plain_text_password).digest()), bcrypt.gensalt(rounds = SALT_ROUNDS))
+class LoginSetService(object):
+    """
+    Called on init, sets the client which is an abstract 'DataSet' on the frontend
+    and a mongodb entry on the backend. It also sets the 'user_id' and 'password' 
+    so that we can save user specific logins.
+    """
+    def __init__(self, user_id, password, login_set_client=DataSet(adapter=MongoEntry)):
+        self.login_set_client = login_set_client
+        self.user_id = user_id
+        if not user_id:
+            raise Exception("user id not provided")
+        self.password = password
+        if not password:
+            raise Exception("password not provided")
 
-def check_password(plain_text_password, hashed_password):
-  # Check hashed password. Using bcrypt, the salt is saved into the hash itself
-  return bcrypt.checkpw(base64.b64encode(hashlib.sha256(plain_text_password).digest()), hashed_password)
+    def get_hashed_password(plain_text_password):
+      # Hash a password for the first time
+      # Using bcrypt, the salt is saved into the hash itself preventing rainbow table attacks
+      # We use sha256 and encode using baase 64 to bypass the max length problems of blowfish
+      return bcrypt.hashpw(base64.b64encode(hashlib.sha256(plain_text_password).digest()), bcrypt.gensalt(rounds = SALT_ROUNDS))
+
+    def check_password(plain_text_password, hashed_password):
+      # Check hashed password. Using bcrypt, the salt is saved into the hash itself
+      return bcrypt.checkpw(base64.b64encode(hashlib.sha256(plain_text_password).digest()), hashed_password)
