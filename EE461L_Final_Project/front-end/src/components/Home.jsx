2ea32a17b271c61be05340b9eb73be6492443e39
Ayaan Mahimwala <ayaanhaider@gmail.com>
2021-04-08 23:07:03 -0500

    data set table component v1

--- EE461L_Final_Project/front-end/src/components/Home.jsx
+++ EE461L_Final_Project/front-end/src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from './Layout'
+import DataSetTable from './DataSetTable'
+import {Flex} from '@chakra-ui/react'
 
 export default function Home() {
     return (
         <Layout>
-            <h2>Home Page</h2>
+            <Flex m='6' w='75%'>
+                <DataSetTable/>
+            </Flex>
         </Layout>
     )
 }
